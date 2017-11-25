e598ef89af06f8358b2f25259b409e77493bdaf7
Gustavo Aguilar <gustavoaguilar91@gmail.com>
2017-11-24 19:50:28 -0600

    uploading an image (avoiding reload and using ajax)

--- source/app.py
+++ source/app.py
@@ -1,3 +1,4 @@
+import os
 import config
 import logging
 from backend.Filters import Filters
@@ -5,14 +6,41 @@ from backend.Operations import Operations
 logging.basicConfig(level=config.logging_level, format='%(levelname)s - %(message)s')
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, send_from_directory
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = config.UPLOAD_PATH
 
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in config.ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['image']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('image.html', source=filename)
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory('data/uploads/', filename)
+
 
 def process_image():
     op = Operations()
@@ -25,5 +53,5 @@ def process_image():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=4321, debug=True)
     # process_image()
--- source/config.py
+++ source/config.py
@@ -4,6 +4,8 @@ import logging
 
 logging_level = logging.INFO
 
+############################################################
+## Directories
 
 # Gets the working directory path
 ROOT_DIR = os.path.abspath(inspect.getfile(inspect.currentframe()))
@@ -11,4 +13,12 @@ ROOT_DIR = '/'.join(ROOT_DIR.split('/')[:-2]) + '/'
 print(ROOT_DIR)
 
 # Directory for images and other data
-DATA_PATH = ROOT_DIR + '/data/'
+DATA_PATH = ROOT_DIR + 'source/data/'
+
+# Directory where the uploaded images are going to be saved
+UPLOAD_PATH = DATA_PATH + 'uploads/'
+
+############################################################
+## Extensions
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
